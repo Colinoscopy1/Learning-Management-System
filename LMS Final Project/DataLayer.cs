dfaf665c1a9d3db022f238f3302006227aaf6cf8
colin.carrier <colin.carrier@B231-10>
2021-12-03 13:47:12 -0700

    Added DataLayer

---
+++ LMS Final Project/DataLayer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace LMS_Final_Project
+{
+    class DataLayer
+    {
+        private string connectionString = "";
+        SqlConnection conn;
+        
+        public DataLayer(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public void CreateSchema()
+        {
+            string CreateDataBasequery = $@"
+                    
+
+                                           ";
+        }
+           
+    }
+
+    }
+}
