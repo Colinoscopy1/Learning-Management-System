c5c84280eb15e71e1db2f1f33ba5d98b06857372
Gerrit Kat <gerrit.kat@infotech.pri>
2021-12-03 14:50:20 -0700

    Added classes

---
+++ LMS Final Project/Course.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LMS_Final_Project
+{
+    public class Course
+    {
+        private string _ClassNumber;
+        private string _Instructor;
+        private string _Building;
+        private string _RoomNumber;
+        private string _Program;
+        private DateTime _classTime;
+        private int _classFrequency;
+        private int _classDuration;
+
+        public Course()
+        {
+
+        }
+    }
+}
---
+++ LMS Final Project/SchoolProgram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LMS_Final_Project
+{
+    class SchoolProgram
+    {
+        private int _ProgramID;
+        private string _ProgramName;
+        private string _Semester;
+        private int _Year;
+        private List<Course> courses;
+
+        public SchoolProgram()
+        {
+
+        }
+    }
+}
